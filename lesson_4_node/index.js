86895a5a070c17cf6ba0b193a5d61e94bd341f8c
galamo <galamouyal88@gmail.com>
2024-08-04 20:58:42 +0300

    a

--- lesson_4_node/index.js
+++ lesson_4_node/index.js
@@ -7,6 +7,21 @@ app.get("/test", (request, response, next) => {
     return response.send("All Good!")
 })
 
+
+const users = [{ user: "adi", pass: "abc123" },
+{ user: "orel", pass: "aaabbb" },
+{ user: "michal", pass: "manahama" }]
+
+app.get("/users", (req, res, next) => {
+    return res.json(users.map(u => u.user))
+})
+
+app.get("/user-exist", (req, res, next) => {
+    console.log(req.query.user)
+    // http://127.0.0.1:3500/user-exist?user=adi
+    //check if the user exist or not!?
+})
+
 app.listen(3500, () => {
     console.log("Listening to PORT, API Is working 3500")
 })
