85f98cdae914db5cb4ec41ccc9b8c2810b3e3cdd
galamo <galamouyal88@gmail.com>
2024-08-07 18:42:44 +0300

    a

---
+++ lesson_5_node/api_1/index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const { logger } = require("./logger")
+const fs = require("node:fs")
+
+const app = express()
+
+let messages = [];
+
+function loadMessagesFromFile() {
+    fs.readFile("./log.txt", "utf8", (err, data) => {
+        if (err) { console.log("error") }
+        messages = data.split("\n")
+    })
+}
+loadMessagesFromFile()
+
+
+app.get("/message", (req, res, next) => {
+
+    const message = req?.query?.message;
+    messages.push(message)
+    logger(message)
+    res.json(messages)
+
+})
+
+app.post("/car", (req, res, next) => {
+    res.send("POST!")
+})
+
+app.listen(4500)
+
+
+// error handling
+// http method
+// middleware
+// TS 
