a2447090c3b3bd77772cdd83a501c24291894519
galamo <galamouyal88@gmail.com>
2024-08-11 18:28:03 +0300

    a

---
+++ lesson_6_node/api/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+
+
+
+
+app.listen(process.env.API_PORT, () => {
+    console.log(`Running on PORT: ${process.env.API_PORT}`)
+})
