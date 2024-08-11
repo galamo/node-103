da793198e214608783da7676161f6351e2cecc69
galamo <galamouyal88@gmail.com>
2024-08-11 19:17:42 +0300

    a

--- lesson_6_node/api/src/index.ts
+++ lesson_6_node/api/src/index.ts
@@ -1,9 +1,29 @@
-import express from "express"
+import express, { NextFunction, Request, Response } from "express"
+
 import dotenv from "dotenv"
 dotenv.config()
 
 const app = express()
 
+app.use((req, res, next) => {
+    console.log("middleware!!!", req.url)
+    next()
+})
+
+app.get("/transactions", (req: Request, res: Response, next) => {
+    // @ts-ignore
+    someParam
+    res.status(200).json([{ id: "t1", amount: 5000 }, { id: "t2", amount: 6000 }])
+})
+app.get("/info", (req: Request, res: Response, next) => {
+    res.status(200).json({ message: "ok" })
+})
+
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log(error.message)
+    res.status(500).send("Something went wrong!")
+})
 
 
 app.listen(process.env.API_PORT, () => {
